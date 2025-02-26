4c805b116b1a7c1e67369da5ffbe33192482c03b
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2025-02-26 18:10:39 -0300

    Aula 9 de 12: Como enviar e-mail com conta secundária no outlook

--- enviar_email_contasecundaria.py
+++ enviar_email_contasecundaria.py
@@ -5,16 +5,18 @@ outlook = win32.Dispatch("outlook.application")
 
 email = outlook.CreateItem(0)
 
+conta = outlook.Session.Accounts["seu_segundo_email_outlook@gmail.com"]
 
+email._oleobj_.Invoke(*(64209, 0, 8, 0, conta))
 
-email.To = "falseta_fake_0@hotmail.com"
-email.Cc = "pythonimpressionador@gmail.com;falseta_fake+copia@hotmail.com"
-email.Bcc = "pythonimpressionador+oculto@gmail.com"
+email.To = "seuemaildestino@hotmail.com"
+email.Cc = "seuemailcopia@gmail.com;outroemailcopia@hotmail.com"
+email.Bcc = "outroemailcopiaoculta@gmail.com"
 email.Subject = "Email enviado pelo Outlook"
 # email.Body = "Texto do email"
 email.HTMLBody = """<p>Meu primeiro paragrafo</p>
 <p>Outro paragrafo no email</p>
-<img src='https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fa6d41686876ceccfc436dd310b9e49aa.cdn.bubble.io%2Ff1658516625802x148010885188176500%2Flogo%2520hash%2520oficial%2520-%2520letra%2520azul.png?w=&h=&auto=compress&dpr=1&fit=max'
+<img src='seu_link_imagem'
 width=200>"""
 
 caminho_codigo = os.getcwd()
